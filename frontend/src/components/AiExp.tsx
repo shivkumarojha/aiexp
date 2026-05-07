ef21d7dcb83dc16773aa652b2ba536c923dbfe9c
shivkumarojha <shivkmojha@gmail.com>
2026-05-07 21:12:01 +0530

    added logout button

--- frontend/src/components/AiExp.tsx
+++ frontend/src/components/AiExp.tsx
@@ -6,7 +6,7 @@ export function AiExp() {
   const { data: session } = authClient.useSession();
   const navigate = useNavigate();
   const handleLogout = async () => {
-    console.log("Inside handleLogout")
+    console.log("Inside handleLogout");
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
@@ -32,10 +32,28 @@ export function AiExp() {
         AI Exp
       </h1>
       {session && (
-        <Button onClick={handleLogout} className="z-20 text-red-800 ml-auto">
-          Logout
+        <Button
+          onClick={handleLogout}
+          className="z-20 text-red-800 size-10 ml-auto"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-logout"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M9 12h12l-3 -3" />
+            <path d="M18 15l3 -3" />
+          </svg>
         </Button>
-
       )}
     </div>
   );
